21596ba258ddde7465612b278af0eaa3dcf0e353
Furkan Topal <frkntopall@outlook.com>
2022-12-31 00:41:26 +0300

    Go to the homepage by typing mail and password

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:shopping_app/screens/edit_product_screen.dart';
-import './providers/orders.dart';
 
+import './screens/edit_product_screen.dart';
 import './screens/cart_screen.dart';
 import './screens/products_overview_screen.dart';
 import './screens/product_detail_screen.dart';
-import 'providers/products.dart';
-import './providers/cart.dart';
 import './screens/orders_screen.dart';
 import './screens/user_products_screen.dart';
 import './screens/auth_screen.dart';
+import './providers/cart.dart';
+import './providers/products.dart';
+import './providers/orders.dart';
+import './providers/auth.dart';
 
 void main() => runApp(const MyHomePage());
 
@@ -20,35 +21,39 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (ctx) => Products(),
-        ),
-        ChangeNotifierProvider(
-          create: (ctx) => Cart(),
-        ),
-        ChangeNotifierProvider(
-          create: (ctx) => Orders(),
-        )
-      ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Shopping',
-        theme: ThemeData(
-          primaryColor: Color.fromRGBO(132, 75, 202, 0.133),
-          fontFamily: 'Lato',
-          colorScheme:
-              ColorScheme.fromSwatch().copyWith(secondary: Colors.deepOrange),
-        ),
-        home: AuthScreen(),
-        routes: {
-          ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
-          CartScreen.routeName: (ctx) => CartScreen(),
-          OrdersScreen.routeName: (ctx) => OrdersScreen(),
-          UserProductsScreen.routeName: (ctx) => UserProductsScreen(),
-          EditProductScreen.routeName: (ctx) => EditProductScreen(),
-        },
-      ),
-    );
+        providers: [
+          ChangeNotifierProvider(
+            create: (context) => Auth(),
+          ),
+          ChangeNotifierProvider(
+            create: (ctx) => Products(),
+          ),
+          ChangeNotifierProvider(
+            create: (ctx) => Cart(),
+          ),
+          ChangeNotifierProvider(
+            create: (ctx) => Orders(),
+          )
+        ],
+        child: Consumer<Auth>(
+          builder: ((ctx, auth, _) => MaterialApp(
+                debugShowCheckedModeBanner: false,
+                title: 'Shopping',
+                theme: ThemeData(
+                  primaryColor: Color.fromRGBO(132, 75, 202, 0.133),
+                  fontFamily: 'Lato',
+                  colorScheme: ColorScheme.fromSwatch()
+                      .copyWith(secondary: Colors.deepOrange),
+                ),
+                home: auth.isAuth ? ProductOverviewScreen() : AuthScreen(),
+                routes: {
+                  ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+                  CartScreen.routeName: (ctx) => CartScreen(),
+                  OrdersScreen.routeName: (ctx) => OrdersScreen(),
+                  UserProductsScreen.routeName: (ctx) => UserProductsScreen(),
+                  EditProductScreen.routeName: (ctx) => EditProductScreen(),
+                },
+              )),
+        ));
   }
 }
